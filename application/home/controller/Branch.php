445a16c23c6387cce698a9c3c96c721c6014d068
wzc <958364865@qq.com>
2017-08-11 09:32:39 +0800

    支部建设  修改

---
+++ application/home/controller/Branch.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laowang <958364865@qq.com>
+ * Date: 2017/8/11
+ * Time: 9:30
+ */
+namespace app\home\controller;
+/**
+ * Class Branch
+ * @package 支部建设
+ */
+class Branch extends Base
+{
+    /**
+     * 主页
+     */
+    public function index(){
+        
+    }
+    
+}
