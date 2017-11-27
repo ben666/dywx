8ce50c7fefd14f1597957c1a93aabe07dda52677
laowang <958364865@qq.com>
2017-11-27 15:24:11 +0800

    默认图片 修改

--- application/home/controller/Base.php
+++ application/home/controller/Base.php
@@ -117,7 +117,7 @@ class Base extends Controller {
      */
     public function default_pic(){
         //随机封面图
-        $a = array('1'=>'a','2'=>'b','3'=>'c','4'=>'d','5'=>'e','6'=>'f','7'=>'g','8'=>'h','9'=>'i','10'=>'j','11'=>'k','12'=>'l','13'=>'m','14'=>'n','15'=>'o');
+        $a = array('4'=>'d','5'=>'e','6'=>'f','7'=>'g','8'=>'h','9'=>'i','10'=>'j');
         $front_pic = array_rand($a,1);
         return $front_pic;
     }
